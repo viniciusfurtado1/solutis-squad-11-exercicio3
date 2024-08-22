6da1df399eaefb43a30d915cb70f3c615bb281df
Daniel Vasconcelos <79517613+danbasco@users.noreply.github.com>
2024-08-22 17:42:01 -0300

    Bug: Fix Percentage error calculation

--- Questao3.java
+++ Questao3.java
@@ -1,29 +1,25 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Questao_3 {
 
-        int homens = 0;
-        int mulheres = 0;
-        int total = 0;
-        float porcH = 0;
-        float porcM = 0;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Informe a quantidades de homens");
-        homens = scan.nextInt();
-        System.out.println("Informe a quantidade de mulheres");
-        mulheres = scan.nextInt();
-        total = homens + mulheres;
-        porcM = (total * mulheres) / 100.f;
-        porcH = (total * homens) / 100.f;
+        public static void main(String[] args) {
 
-        System.out.println("total de alunos na faculdade " + total);
-        System.out.println(" Porcentagem Homens " + porcH+ "%");
-        System.out.println(" Porcentagem Mulheres " + porcM+ "%");
+            int homens = 0;
+            int mulheres = 0;
 
-    }
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Informe a quantidades de homens");
+            homens = scan.nextInt();
+            System.out.println("Informe a quantidade de mulheres");
+            mulheres = scan.nextInt();
+
+            System.out.printf("Porcentagem Homens: %.2f%c\n",
+                    ((float)homens/(homens+mulheres)*100), '%');
+            System.out.printf("Porcentagem Mulheres: %.2f%c\n",
+                    ((float)mulheres/(homens+mulheres)*100), '%');
+
+            scan.close();
+
+        }
 }
-
-
-
-
