82cc6d3399f303cfb08f720a1eb2b61c6426bad6
Vinicius Furtado <vinicius_furtado10@hotmail.com>
2024-08-22 18:20:32 -0300

    Update Questao51.java

--- Questao51.java
+++ Questao51.java
@@ -1,41 +1,25 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        long num1 = 1, num2 = 1;
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Digite o número de termos da sequência de Tribonacci: ");
+        int n = input.nextInt();
 
-        System.out.println(num1);
-        System.out.println(num2);
-
-
-        for (int i = 3; i <= 50; i++) {
-            long proximoNumero = num1 + num2;
-            System.out.println(proximoNumero);
-
-
-            num1 = num2;
-            num2 = proximoNumero;
-        }
-        System.out.print("\nDigite o n-ésimo termo da sequência de Fibonacci que você deseja ver: ");
-        int n = scanner.nextInt();
-
-        // Calculando o n-ésimo termo da sequência de Fibonacci
-        num1 = 1;
-        num2 = 1;
-
-        if (n == 1 || n == 2) {
-            System.out.println("O " + n + "º termo da sequência de Fibonacci é: 1");
+        if (n < 3) {
+            System.out.println("O número de termos deve ser pelo menos 3.");
         } else {
-            for (int i = 3; i <= n; i++) {
-                long proximoNumero = num1 + num2;
-                num1 = num2;
-                num2 = proximoNumero;
+            int t1 = 1, t2 = 1, t3 = 2;
+            System.out.print(t1 + " " + t2 + " " + t3 + " ");
+
+            for (int i = 4; i <= n; i++) {
+                int nextTerm = t1 + t2 + t3;
+                System.out.print(nextTerm + " ");
+                t1 = t2;
+                t2 = t3;
+                t3 = nextTerm;
             }
-            System.out.println("O " + n + "º termo da sequência de Fibonacci é: " + num2);
         }
-        scanner.close();
     }
 }
