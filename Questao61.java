515b9c477c989278a6912dbe6476fa3bb6d169be
Marcos Vinícius <142227095+marcosvcg@users.noreply.github.com>
2024-08-22 18:23:05 -0300

    BUG: corrige o erro da implementação da questão anterior ao número

--- Questao61.java
+++ Questao61.java
@@ -5,35 +5,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("###,##0.00");
+        String piramide = "*";
 
-        System.out.print("Digite o valor inicial da dívida: R$ ");
-        double divida = scanner.nextDouble();
+        System.out.print("Informe a quantidade de linhas da pirâmide: ");
+        int quantidadeDeLinhas = scanner.nextInt();
 
-        System.out.print("Digite a taxa de correção mensal (%): ");
-        double taxaCorrecao = scanner.nextDouble() / 100;
-
-        System.out.print("Digite a quantidade de anos: ");
-        int anos = scanner.nextInt();
-
-        System.out.println("\nEvolução da dívida ao longo dos meses e anos:");
-
-        System.out.print("Ano\t");
-        String[] meses = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
-        for (String mes : meses) {
-            System.out.print(mes + "\t");
-        }
-        System.out.println();
-
-        for (int ano = 2007; ano < 2007 + anos; ano++) {
-            System.out.print(ano + "\t");
-            for (int mes = 0; mes < 12; mes++) {
-                // Aplica a taxa de correção ao valor da dívida
-                divida += divida * taxaCorrecao;
-                System.out.print(df.format(divida) + "\t");
-            }
+        for(int i = 1; i <= quantidadeDeLinhas; i++) {
+            System.out.print(piramide.repeat(i));
             System.out.println();
         }
 
