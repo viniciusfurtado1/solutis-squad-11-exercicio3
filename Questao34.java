35f76be48f744040e186a23d8cdaa261fabd5508
Marcos Vinícius <142227095+marcosvcg@users.noreply.github.com>
2024-08-22 18:07:14 -0300

    BUG: corrige o erro da implementação da questão anterior ao número

--- Questao34.java
+++ Questao34.java
@@ -1,11 +1,9 @@
 public class NumerosPares {
     public static void main(String[] args) {
-        // Loop de 10 até 200
-        for (int i = 10; i <= 200; i++) {
-            // Verifica se o número é par
-            if (i % 2 == 0) {
-                System.out.println(i);
-            }
+        Random random = new Random();
+        for (int i = 0; i < 50; i++) {
+            int numero = random.nextInt(1, 101);
+            System.out.println(numero);
         }
     }
 }
