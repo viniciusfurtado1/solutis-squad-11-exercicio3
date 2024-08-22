7bca7a6eb961b2237db83ef662d7c42ea06d0e41
Vinicius Furtado <vinicius_furtado10@hotmail.com>
2024-08-22 18:22:54 -0300

    Update Questao59.java

--- Questao59.java
+++ Questao59.java
@@ -1,27 +1,11 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        DateTimeFormatter formatoData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        System.out.print("Digite a data (formato dd/MM/yyyy): ");
-        String dataInput = scanner.nextLine();
-
-        LocalDate data = LocalDate.parse(dataInput, formatoData);
-
-        System.out.print("Digite a quantidade de dias a ser somada: ");
-        int dias = scanner.nextInt();
-
-        LocalDate novaData = data.plusDays(dias);
-
-        System.out.println("A nova data é: " + novaData.format(formatoData));
-
-        scanner.close();
-
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Tabuada do " + i + ":");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println();
+        }
     }
 }
