ad3ab155275ae9d9a46c97ad0c62f51f4b5b2615
Marcos Vinícius <142227095+marcosvcg@users.noreply.github.com>
2024-08-22 21:04:14 -0300

    Update Questao59.java

--- Questao59.java
+++ Questao59.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Questao59 {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Tabuada do " + i + ":");
